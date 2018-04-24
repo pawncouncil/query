982639ec1f5fb09b681e8c78e3ba9c2207992c42
Paul Turner <pt@Our-MacBook-Pro.local>
2018-04-24 13:21:07 -0700

    Our combined jquery practice.

---
+++ jquery.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $(".click").click(function(){
+        // console.log("poop");
+        alert("You clicked a button!");
+    })
+    $(".hide").click(function(){
+        $(".hp").hide();
+    })
+})
+
